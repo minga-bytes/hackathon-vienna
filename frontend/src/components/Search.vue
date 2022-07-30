e9a04cf52a3df575bebcb2388f4d5e07fa768a1c
dominikr_spheos <dominik.rakowski@spheos.com>
2022-07-30 16:48:07 +0200

    added search componente

---
+++ frontend/src/components/Search.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="searchform-container">
+    <form class="d-flex" role="search">
+      <input
+        class="form-control me-2"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+      />
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.searchform-container {
+  margin-left: 1em;
+  margin-right: 1em;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import Navbar from '@/components/Navbar.vue'
+import Search from '@/components/Search.vue'
 </script>
 
 <template>
   <main>
     <Navbar />
+    <br>
+    <Search />
   </main>
 </template>
+
