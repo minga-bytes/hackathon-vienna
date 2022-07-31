463b96976ca56a4e32c974c3433825e4196adac0
Timm Werner <timm.werner@schmerreim.de>
2022-07-31 10:56:55 +0200

    Add data import EP

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,7 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 var app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '70mb'}));
+app.use(bodyParser.urlencoded({limit: '70mb', extended: true}));
 
 import pkg from 'pg';
 const { Pool, Client } = pkg;
@@ -24,9 +25,18 @@ app.get('/user', function (req, res) {
 });
 
 
-app.post('/import_street_data', function (req, res) {
-    const polygon = req.body.polygon;
-    res.send('Hello POST'+polygon.x );
+app.post('/import_street_data', async function (req, res) {
+    const features = req.body.features;
+
+    const promises = features.map(async (feature)=>{
+        if (feature.geometry) {
+            await client.query(`INSERT INTO public.street_data VALUES (ST_GeomFromGeoJSON('${JSON.stringify(feature.geometry)}'));`);
+        }
+    });
+
+    await Promise.all(promises)
+
+    res.send('Hello POST' );
 });
 
 var server = app.listen(8081, function () {
